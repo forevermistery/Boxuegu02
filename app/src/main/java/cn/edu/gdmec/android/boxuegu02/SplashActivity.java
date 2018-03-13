f267f8d63fd7d7f2163045dd55495b29fd70223c
forevermistery <1258234227@qq.com>
2018-03-13 17:18:34 +0800

    1

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu02/SplashActivity.java
@@ -0,0 +1,36 @@
+package cn.edu.gdmec.android.boxuegu02;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by apple on 18/3/13.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        
+        Timer timer=new Timer();
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+
+            }
+        };
+        timer.schedule(task,3000);
+
+    }
+
+
+}
